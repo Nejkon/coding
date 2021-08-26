6491f1449a167880737673f2d2ff4de134c3cef8
Nikita Telenkov <89317222+Nejkon@users.noreply.github.com>
2021-08-26 12:13:30 +0300

    Update ex8new.c

--- ex8new.c
+++ ex8new.c
@@ -1,1 +1,64 @@
+//ex8new.c - упражнение 8, глава 2, Стивен Прата. Некоторые изменения, выходящие за рамки д. з.
+
+/*===============================================================================*/
+
+/*изначальный код восьмого упражнения второй главы из книги
 #include <stdio.h>
+
+void one_three(void);
+void two(void);
+
+int main() 
+{
+
+   printf("начинаем:\n");
+   one_three();
+   printf("порядок!\n");
+   
+    
+return 0;
+}
+
+
+void one_three(void)
+{printf("один\n");two();printf("три\n");}
+  void two(void)
+    {printf("два\n");}*/
+
+/*===============================================================================*/
+
+//несколько изменённая версия
+
+#include <stdio.h>
+
+int main(void);
+void one_three(void);
+void two(void);
+void pseudomain(void); //замена деятельности main() другой функцией "псевдо-мейн"
+void agrmain(void); // вызов main() из другой функции "агр-мейн"
+
+int main(void) 
+{printf("старт\n");
+    pseudomain();
+    agrmain();
+}
+
+void pseudomain(void)
+{
+   printf("начинаем:\n");
+   one_three();
+   printf("порядок!\n");
+   
+}
+
+
+void one_three(void)
+{printf("один\n");two();printf("три\n");}
+  void two(void)
+    {printf("два\n");}
+    
+//бесконечная программа
+
+void agrmain(void)
+{
+main();}
